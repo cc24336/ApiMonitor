4d2d7f391650b456983a2fc6d25b30e0575649af
Thiago Gomes <cc24336@g.unicamp.br>
2025-06-02 22:23:25 -0300

    up

--- MonitorApi/monitores.cs
+++ MonitorApi/monitores.cs
@@ -1,13 +1,8 @@
-namespace APIMonitorVesp
+namespace MonitorApi
 {
     public class Monitor
     {
-        private string Ra;
-        private string? nome;
-
         public int Ra { get; set; }
         public required string Nome { get; set; }
-
-        
     }
 }
